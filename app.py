7882e6ce39db37c21177a7e2e9fce46a4c042401
Rodriasdev <veronrodrigo98@gmail.com>
2024-07-31 03:17:51 -0300

    was enabled the cors

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
-from openai import OpenAI
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from openai import OpenAI
 
+app = Flask(__name__)
+CORS(app)
 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
-
-
-app = Flask(__name__)
-
 @app.route('/chat', methods=['POST'])
 def chat():
+    data = request.get_json()
+    print(data)
 
-  data = request.get_json()
+    try:
+        completion = client.chat.completions.create(
+            model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
+            messages=[
+                {"role": "system", "content": "Eres un asistente que responde preguntas sobre la salud mental, no respondas nada que no tenga que ver con la salud mental."},
+                {"role": "user", "content": data["msg"]}
+            ],
+            temperature=0.7,
+            max_tokens=500
+        )
+        answer = completion.choices[0].message.content
+    except Exception as e:
+        print(f"Error: {e}")
+        answer = "Hubo un error al procesar tu solicitud."
 
-
-  completion = client.chat.completions.create(
-    model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
-    messages=[
-      {"role": "system", "content": "Eres un asistente que responde preguntas sobre la salud mental."},
-      {"role": "user", "content": data["msg"]}
-    ],
-    temperature=0.7,
-    max_tokens=500
-  )
-
-  return jsonify({"answer":completion.choices[0].message.content})
-
+    return jsonify({"answer": answer})
 
 if __name__ == "__main__":
-  app.run(debug=True)
-
-
-
+    app.run(debug=True)
 
