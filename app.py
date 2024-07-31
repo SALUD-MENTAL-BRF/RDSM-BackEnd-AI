cdc1fe7d59e9b4256a8b1d3fe5c249f79c9c147b
Rodriasdev <veronrodrigo98@gmail.com>
2024-07-30 21:51:24 -0300

    was added the route for the chat

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from openai import OpenAI
+from flask import Flask, jsonify, request
 
 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
-completion = client.chat.completions.create(
-  model="TheBloke/Llama-2-7B-GGUF",
-  messages=[
-    {"role": "system", "content": "Eres un asistente que responde preguntas sobre la salud mental."},
-    {"role": "user", "content": "Que es el neurodesarrollo?"}
-  ],
-  temperature=0.7,
-  max_tokens=500
-)
 
-print(completion.choices[0].message)
+
+app = Flask(__name__)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+
+  data = request.get_json()
+
+
+  completion = client.chat.completions.create(
+    model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
+    messages=[
+      {"role": "system", "content": "Eres un asistente que responde preguntas sobre la salud mental."},
+      {"role": "user", "content": data["msg"]}
+    ],
+    temperature=0.7,
+    max_tokens=500
+  )
+
+  return jsonify({"answer":completion.choices[0].message.content})
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+
 
