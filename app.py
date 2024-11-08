e4509bf9796107a795110cff3b4abc230d1717ef
Rodriasd <veronrodrigo98@gmail.com>
2024-11-08 16:42:12 -0300

    changed propmt

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from social_hability import activity_social_hability
-# from openai import OpenAI
 
 app = Flask(__name__)
 CORS(app)
 
-# client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
-# @app.route('/chat', methods=['POST'])
-# def chat():
-#     data = request.get_json()
-#     print(data)
-
-#     try:
-#         completion = client.chat.completions.create(
-#             model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
-#             messages=[
-#                 {"role": "system", "content": "Eres un asistente que responde preguntas sobre la salud mental, no respondas nada que no tenga que ver con la salud mental."},
-#                 {"role": "user", "content": data["msg"]}
-#             ],
-#             temperature=0.7,
-#             max_tokens=500
-#         )
-#         answer = completion.choices[0].message.content
-#     except Exception as e:
-#         print(f"Error: {e}")
-#         answer = "Hubo un error al procesar tu solicitud."
-
-#     return jsonify({"answer": answer})
 
 
 @app.route('/hability-social', methods=['POST'])
 def hability_social():
     data = request.get_json()
     try:
-        answer = activity_social_hability(data["genre"], data["stage"])
+        answer = activity_social_hability(data["age"],data["genre"], data["stage"], data["complexity"], data["personality"])
     except Exception as e:
         print(f"Error: {e}")
         answer = "Hubo un error al procesar tu solicitud"
--- social_hability.py
+++ social_hability.py
@@ -1,3 +1,47 @@
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_ollama.llms import OllamaLLM
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "Eres un asistente que genera simulaciones de conversaciones para mejorar habilidades sociales en diferentes escenarios."),
+#     ("user", "{question}")
+# ])
+
+# model = OllamaLLM(model="llama3.2:3b-instruct-q8_0")
+
+# chain = prompt | model
+
+
+# def activity_social_hability(age,genre, stage):
+#     respuesta = chain.invoke({
+#         "question": f"""
+#         Crea un escenario social simple acorde a mi edad de {age} años, en el cual estoy en {stage} y soy de género {genre}. 
+#         Genera **tres respuestas posibles**: una debe ser correcta y las otras dos incorrectas. 
+#         Es **crucial** que incluyas todas las respuestas y que sigas estrictamente esta estructura:
+
+#         - Escenario: "Descripción clara del escenario."
+#         - Respuestas:
+#           1) "Primera respuesta incorrecta."
+#           2) "Respuesta correcta."
+#           3) "Segunda respuesta incorrecta."
+#         - Correcta: INDICA EL NÚMERO DE LA RESPUESTA CORRECTA (1, 2 o 3).
+#         - Explicación: "Explica por qué esta es la opción correcta en una o dos frases."
+
+#         **Ejemplo de formato correcto:**
+#         Escenario: "Estás en una reunión de trabajo y tu jefe te pide que presentes un informe."
+#         Respuestas:
+#         1) "Te disculpas y dices que no tienes el informe listo."
+#         2) "Presentas el informe tal como te lo han pedido."
+#         3) "Dices que otra persona se encargará de la presentación."
+#         Correcta: 2
+#         Explicación: "La opción 2 es correcta porque muestra responsabilidad y respeto hacia tu jefe al estar preparado."
+
+#         **Asegúrate de que el formato sea muy claro y que no haya variaciones en la estructura.** 
+#         """
+#     })
+
+#     return respuesta
+
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 
@@ -11,10 +55,10 @@ model = OllamaLLM(model="llama3.2:3b-instruct-q8_0")
 chain = prompt | model
 
 
-def activity_social_hability(genre, stage):
+def activity_social_hability(age,genre, stage, complexity, personality):
     respuesta = chain.invoke({
         "question": f"""
-        Crea un escenario social simple acorde a mi edad de 18 años, en el cual estoy en {stage} y soy de género {genre}. 
+        Crea un escenario social {complexity} acorde a mi edad de {age} años, en el cual estoy en {stage}, soy de género {genre} y personalidad es {personality}. 
         Genera **tres respuestas posibles**: una debe ser correcta y las otras dos incorrectas. 
         Es **crucial** que incluyas todas las respuestas y que sigas estrictamente esta estructura:
 
